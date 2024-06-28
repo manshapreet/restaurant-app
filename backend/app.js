e9cf75ead3164030cc59bd817b3c5d789f2b1648
Mansha <manshapreet02@gmail.com>
2024-06-29 01:17:17 +0530

    server setup

---
+++ backend/app.js
@@ -0,0 +1,88 @@
+//npm init
+//npm i express dotenv hbs mongo mongoose nodemon body-parser express-session connect-mongodb-session
+
+
+
+require("dotenv").config();
+
+const path = require('path');
+const express = require('express');
+const app= express();
+const PORT = process.env.PORT || 4700;
+const bodyparser = require('body-parser');//use with axios 
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+app.use(cors( {
+origin: 'http://localhost:3000', credentials: true,
+withCredentials: true
+}))
+
+
+const mongoose = require('mongoose');
+
+
+const session = require('express-session')
+const MongoDBsession = require('connect-mongodb-session')(session);//use with session
+
+const store = new MongoDBsession({
+    uri: process.env.MONGODB_URL,
+    collection: "mysessions"
+});
+
+app.use(cookieParser())
+
+app.use(
+    session({
+        secret:'secret key for cookie',
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+        cookie:{secure:false}
+    })
+);
+
+
+
+
+app.set('view engine','hbs');
+
+
+app.use(express.urlencoded({ extended: true }));
+//app.use(bodyparser.json()); 
+app.use(bodyparser.json({limit: "50mb"}));
+app.use(bodyparser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'photos')));
+
+
+
+
+const AuthRouter = require('./routes/authentication.js');
+app.use('/', AuthRouter);
+
+
+    
+// app.listen(PORT, () => {
+//     console.log(`http://localhost:` + PORT);
+// })
+
+
+
+
+//////USE BELOW CODE WITH MONGOOSE  
+//////
+
+mongoose.connect(process.env.MONGODB_URL ,{
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+   // useCreateIndex: true
+})
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`http://localhost:` + PORT);
+        })
+    })
+    .catch(err => {console.error(err);});
+
